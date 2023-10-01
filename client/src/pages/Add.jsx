2fc7a373babf3f8603725fafd70d02b10c5a12fd
Tafita Hery <tafitahery@gmail.com>
2023-10-01 19:49:03 +0300

    React Add New Data to MySql Databas

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,3 +1,64 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Add() {
-  return <div>Add</div>;
+  const [book, setBook] = useState({
+    title: '',
+    desc: '',
+    price: 0,
+    cover: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:8800/books', {
+        ...book,
+        price: parseInt(book.price),
+      });
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <h1>Add New Book</h1>
+        <input
+          name="title"
+          type="text"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input
+          name="desc"
+          type="text"
+          placeholder="desc"
+          onChange={handleChange}
+        />
+        <input
+          name="price"
+          type="number"
+          placeholder="price"
+          onChange={handleChange}
+        />
+        <input
+          name="cover"
+          type="text"
+          placeholder="cover"
+          onChange={handleChange}
+        />
+        <button onClick={handleClick}>Add</button>
+      </form>
+    </div>
+  );
 }
