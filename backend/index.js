85ae43d6d8c442059c3f25d4236416441f2c364f
Tafita Hery <tafitahery@gmail.com>
2023-10-01 16:32:20 +0300

    Node.js Mysql Fetch Data (read)

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,17 @@ app.get('/', (req, res) => {
   res.json('Hello this is the backend');
 });
 
+// If there is a auth problem
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456'
+
+app.get('/books', (req, res) => {
+  const q = 'SELECT * FROM books';
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
 app.listen(8800, () => {
   console.log('Connected to backend!');
 });
