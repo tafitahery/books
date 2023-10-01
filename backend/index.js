b5dadc22d93c6d283dd50f5820116be79a0988b3
Tafita Hery <tafitahery@gmail.com>
2023-10-01 17:14:54 +0300

    Node.js MySql Create a New Data

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
+app.use(express.json());
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -22,6 +23,16 @@ app.get('/books', (req, res) => {
   db.query(q, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
+  });
+});
+
+app.post('/books', (req, res) => {
+  const q = 'INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)';
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json('Book has been created successfully');
   });
 });
 
