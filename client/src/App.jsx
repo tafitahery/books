1107bfc7a38b1c67d70eeecd86f361d99679f823
Tafita Hery <tafitahery@gmail.com>
2023-10-02 13:38:14 +0300

    React App Design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Books from './pages/Books';
 import Add from './pages/Add';
 import Update from './pages/Update';
+import './style.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Books />} />
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -27,6 +27,8 @@ export default function Books() {
             <h2>{book.title}</h2>
             <p>{book.desc}</p>
             <span>{book.price}</span>
+            <button className="delete">Delete</button>
+            <button className="update">Update</button>
           </div>
         ))}
       </div>
